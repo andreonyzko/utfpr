d283ec8e8f3543866877d949f811ef2a00bc5e8b
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2024-04-13 04:23:51 -0300

    aula03

--- utfpr/AULA03/ex004.c
+++ utfpr/AULA03/ex004.c
@@ -7,10 +7,10 @@ int main(){
     scanf("%d %f %f %f", &h2o, &tarifah2o, &tarifaesg, &tributosf);
 
     tarifah2o = h2o*tarifah2o;
-    tarifaesg= h2o*tarifaesg/100;
+    tarifaesg= tarifah2o*tarifaesg/100;
     h2oesg = tarifah2o+tarifaesg;
     tributosf = h2oesg*tributosf/100;
     total= h2oesg+tributosf;
 
-    printf("%f %f %f %f %f", tarifah2o, tarifaesg, h2oesg, tributosf, total);
+    printf("Total Agua: %.2f\nTotal Esgoto: %.2f\nTotal: %.2f\nTributos Federais: %.2f\nVALOR DA FATURA: %.2f", tarifah2o, tarifaesg, h2oesg, tributosf, total);
 }
