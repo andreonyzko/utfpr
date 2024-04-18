9b1cbcdd5e15a767964819259e95bb644a52d15e
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2024-04-17 22:19:29 -0300

    class05 fixs

--- utfpr/AULA05/ex002.c
+++ utfpr/AULA05/ex002.c
@@ -5,11 +5,10 @@ int main(){
     while(1){
         scanf("%d", &n);
 
-        if(n < 1000 && n > 2){
-            break;
-        } else{
+        if(n < 1000 || n > 2)
             printf("Valor invalido! Tente novamente!\n");
-        }
+        else
+            break;
     }
 
     for(int i=1; i<=10; i++){
