44691b5d732684a983e30622ad17192efb0c1c84
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2025-02-18 15:51:56 -0300

    AULA 08 ex004

--- Algorithms-II/AULA08/ex004.c
+++ Algorithms-II/AULA08/ex004.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+void gravaNoArquivo(FILE *fp, int v[], int n){
+    for(int i=0; i < n; i++){
+        fprintf(fp, "%d ", v[i]);
+    }
+}
+
+int somaArquivo(FILE *fp){
+    fseek(fp, 0, SEEK_SET);
+    int buffer, soma=0;
+    while(fscanf(fp, "%d", &buffer) != EOF){
+        soma += buffer;
+    }
+    return soma;
+}
+
 int main(){
-    
+    int v[] = {1,2,3,4,5};
+    FILE *fp = fopen("arquivo.txt","w+");
+    gravaNoArquivo(fp,v,5);
+    printf("Soma = %d\n",somaArquivo(fp));
+    fclose(fp);
 }
